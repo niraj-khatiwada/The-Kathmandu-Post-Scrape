61f874b133d58bec90d2bf5a5f8c2ec4249a4e4c
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-20 17:55:56 +0545

    Improved

--- The_Kathmandu_Post_Scrape/scrape_app/views.py
+++ The_Kathmandu_Post_Scrape/scrape_app/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-import os, sys
-from subprocess import run, PIPE
 from .models import TheKathmanduPostScrapeModel
+from django.contrib import messages
 import datetime
 import re
 # Create your views here.
 
 
 def scrape(request):
-    context={
-        'items': TheKathmanduPostScrapeModel.objects.filter(crawled_time__contains = current_datetime()).distinct("link").all(),
-        
-    }
+    items_query = TheKathmanduPostScrapeModel.objects.filter(crawled_time__contains = current_datetime()).distinct("link")
+    if items_query:
+        context={
+            'items': items_query
+            
+        }
+    else:
+        messages.add_message(request, messages.ERROR, "No data found. You need to crawl first")
+        return render(request, "homepage.html")
+
     return render(request, "homepage.html", context)
 
 def current_datetime():
